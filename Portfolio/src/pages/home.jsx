ca41dee8c5afdc5c05b50e2e2b75f3a1f84b8b7d
Carrnage <Carrnage4@gmail.com>
2024-04-18 13:44:19 +1200

    Revert ""hello world" added to main"
    This reverts commit c9b2f1757e6174034b4fcbefb721a7644cbf299c.

--- Portfolio/src/pages/home.jsx
+++ Portfolio/src/pages/home.jsx
@@ -3,9 +3,6 @@ import { Outlet, Link } from "react-router-dom"
 
 export default function Home() {
     return (
-        <div id="detail">
-            <Outlet />
-            Hello World
-            </div>
+    <div id="detail"><Outlet /></div>
     )
 }
