c9b2f1757e6174034b4fcbefb721a7644cbf299c
Carrnage <Carrnage4@gmail.com>
2024-04-18 13:34:42 +1200

    "hello world" added to main

--- Portfolio/src/pages/home.jsx
+++ Portfolio/src/pages/home.jsx
@@ -3,6 +3,9 @@ import { Outlet, Link } from "react-router-dom"
 
 export default function Home() {
     return (
-    <div id="detail"><Outlet /></div>
+        <div id="detail">
+            <Outlet />
+            Hello World
+            </div>
     )
 }
