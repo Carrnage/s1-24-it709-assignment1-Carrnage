95f4cde740fa58458bd3bf7eefcc7bcde376c20f
Carrnage <Carrnage4@gmail.com>
2024-04-18 13:32:03 +1200

    Upper and lower case weirdness

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom"
-import Home from './pages/home';
+import Home from './pages/Home';
 import Errorpage from './pages/Errorpage';
 import About from './pages/about';
 
